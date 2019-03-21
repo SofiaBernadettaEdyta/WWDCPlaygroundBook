29b5c23d7b20ce9159649a8a828ead87490aeb16
Zofia Drabek <zosia.drabek@gmail.com>
2019-03-21 23:09:32 +0100

    text in playground

---
+++ PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/BalerinaPage.playgroundpage/Contents.swift
@@ -0,0 +1,23 @@
+//#-hidden-code
+//
+//  See LICENSE folder for this template’s licensing information.
+//
+//  Abstract:
+//  The Swift file containing the source code edited by the user of this playground book.
+//
+//#-end-hidden-code
+/*:
+ 
+ # Have you ever thought what physics and dance have in common?
+ 
+ Look at the pictures, can you see the difference between ballerina's positions? How do you think in which case rotational speed is higher?
+ 
+ On the first one ballerina has higher "moment of inertia", because she has her arms and legs extended. And on the second picture ballerina refracted her leg and hand.
+ 
+ In physics there is "Conservation of angular momentum", and because of that if the moment of inertia increases the rotational speed decreases and opposite if the moment of inertia decreases the rotational speed increases.
+ 
+ So if ballerina changed her position from 1 to 2 her rotational speed is increased.
+ 
+
+
+*/
---
+++ PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/XmasTreePage.playgroundpage/Contents.swift
@@ -0,0 +1,20 @@
+//#-hidden-code
+//
+//  See LICENSE folder for this template’s licensing information.
+//
+//  Abstract:
+//  The Swift file containing the source code edited by the user of this playground book.
+//
+//#-end-hidden-code
+
+/*:
+ 
+ #Xmas Tree
+ 
+ Physics is fun, so you observe Conservation of angular momentum by changing position of bubles on the Xmass Tree.
+ 
+ If you put bubles closer to the rotating axis moment of inertia decreases.
+ 
+ Let's try!
+ 
+ */
